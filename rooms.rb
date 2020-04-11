a73dd2b9e5be5282684a59e499eeabc9c1bdca1c
Paulo Rocha <paulorocha1988@gmail.com>
2020-04-11 12:46:15 +0100

    finished mvp

--- rooms.rb
+++ rooms.rb
@@ -4,7 +4,16 @@ attr_reader(:name, :capacity, :music_library)
 
     def initialize(name, capacity, music_library)
         @name = name
-        @capacity = capacity
+        @capacity = 0
         @music_library = []
     end
+
+    def add_song(song)
+        @music_library << song
+    end
+
+    def add_guest(guest)
+        @capacity += 1
+    end
+
 end
--- specs/rooms_specs.rb
+++ specs/rooms_specs.rb
@@ -2,11 +2,15 @@ require('minitest/autorun')
 require('minitest/reporters')
 MiniTest::Reporters.use! MiniTest::Reporters::SpecReporter.new
 require_relative('../rooms.rb')
+require_relative("../songs.rb")
+require_relative("../guests.rb")
 
 class RoomsTest < MiniTest::Test
 
     def setup
-        @room1 = Rooms.new("Boogey Town", 6, [])
+        @song1 = Songs.new("Bohemian Rhapsody", "Queen", "Rock")
+        @room1 = Rooms.new("Boogey Town", 0, [])
+        @guest1 = Guests.new("Paulo", 31)
     end
 
        def test_room_has_name()
@@ -14,13 +18,25 @@ class RoomsTest < MiniTest::Test
        end
 
        def test_room_has_capacity()
-           assert_equal(6, @room1.capacity())
+           assert_equal(0, @room1.capacity())
        end
 
        def test_room_has_a_music_library()
            assert_equal([], @room1.music_library())
        end
    
+       def test_music_library_is_empty()
+            assert_equal(0, @room1.music_library.length())
+       end
 
+       def test_add_song_to_library()
+            @room1.add_song(@song1)
+            assert_equal(1, @room1.music_library.length())
+       end
+
+       def test_add_guest_to_room()
+        @room1.add_guest(@guest1)
+        assert_equal(1, @room1.capacity())
+       end
 
 end
